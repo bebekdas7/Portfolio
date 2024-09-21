943e74a44df440b92b355e1c53108a16a03d3abf
bebekdas7 <bebekdas7gmail.com>
2024-09-20 23:29:49 -0700

    new commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',           // Bind to 0.0.0.0 for external access
+    port: process.env.PORT || 3000  // Use the PORT environment variable or default to 3000
+  }
 })
